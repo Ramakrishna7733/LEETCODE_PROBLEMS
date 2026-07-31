41054d2f19e71e0284b23292412c08907359d14d
MANIVELTHI RAMAKRISHNA RAJU <2400031163@kluniversity.in>
2026-07-31 23:35:02 +0530

    Pascal's Triangle

---
+++ Day-29/solution118.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>r;
+        for(int i=0;i<numRows;i++)
+        {
+            vector<int>rows(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+               rows[j]=r[i-1][j-1]+r[i-1][j];
+
+            }
+            r.push_back(rows);
+        }
+        return r;
+    }
+};
