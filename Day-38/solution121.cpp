d9c41a82c3b8cca505b75c8e6030c4c5f2888c97
MANIVELTHI RAMAKRISHNA RAJU <2400031163@kluniversity.in>
2026-08-09 23:40:22 +0530

    Best Time to Buy and Sell Stock
    Implement maxProfit function to calculate maximum profit from stock prices.

---
+++ Day-38/solution121.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int b = prices[0];
+        int p = 0;
+        int n=prices.size();
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < b) {
+                b = prices[i];
+            } else if (prices[i] - b> p) {
+                p = prices[i] - b;
+            }
+        }
+        return p;
+    }
+    
+};
