2bdaeb9d4563c5ccc6ed816caf6088b52585ccc6
MANIVELTHI RAMAKRISHNA RAJU <2400031163@kluniversity.in>
2026-08-16 23:23:31 +0530

    Sign of the Product of an Array

---
+++ Day-45/solution1822.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+              return 0;
+            }
+            if(nums[i]<0)
+            {
+                c++;
+            }
+
+        }
+        return c%2==0?1:-1;
+    }
+};
