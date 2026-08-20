184e12a69713960a551355516ba9c865e6ff278d
MANIVELTHI RAMAKRISHNA RAJU <2400031163@kluniversity.in>
2026-08-20 22:37:13 +0530

    First Unique Character in a String

---
+++ Day-49/solution387.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int f[26]={0};
+        for(char ch:s)
+        {
+            f[ch-'a']++;
+        }
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(f[s[i]-'a']==1)
+            {
+                return i;
+            }
+             
+           
+        }
+        return -1;
+    }
+};
