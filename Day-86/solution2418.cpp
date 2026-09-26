fbca240d998b72c0cb67f72184a8b82179fdc9ec
MANIVELTHI RAMAKRISHNA RAJU <2400031163@kluniversity.in>
2026-09-26 23:30:08 +0530

    Sort the People

---
+++ Day-86/solution2418.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int n = heights.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (heights[j] > heights[i]) {
+                    swap(heights[j], heights[i]);
+                    swap(names[j], names[i]);
+                }
+            }
+        }
+
+        return names;
+    }
+};
